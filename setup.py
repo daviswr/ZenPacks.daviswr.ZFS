45675397599d2ff85b0ed3a32c9a6e68fb099f17
Wes Davis <wesley.r.davis@gmail.com>
2016-10-08 00:35:05 -0400

    0.7.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.daviswr.ZFS"
-VERSION = "0.7.3"
+VERSION = "0.7.4"
 AUTHOR = "Wes Davis"
 LICENSE = "MIT"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.daviswr']
